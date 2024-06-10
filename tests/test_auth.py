1b7779dbe5aa3f92cb5c0aa938b9f4cdfa5c310f
Rohith Surya Podugu <prsurya1020@gmail.com>
2024-06-10 12:20:54 -0700

    change auth class according to PEP-8 naming conventions

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,10 @@
 # Unit tests to test the authentication module
 import unittest
-from pyhpcc.auth import auth
-from pyhpcc.errors import HPCCAuthenticationError
+
 import config
+
+from pyhpcc.auth import Auth
+from pyhpcc.errors import HPCCAuthenticationError
 
 
 class TestAuth(unittest.TestCase):
@@ -17,7 +19,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_url method
     def test_get_url(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST,
             self.HPCC_PORT,
             self.HPCC_USERNAME,
@@ -31,7 +33,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_username method
     def test_get_username(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST,
             self.HPCC_PORT,
             self.HPCC_USERNAME,
@@ -43,7 +45,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_verified method
     def test_get_verified(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST,
             self.HPCC_PORT,
             self.HPCC_USERNAME,
@@ -55,7 +57,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_verified method with an invalid username and password
     def test_get_verified_invalid_username_password(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST,
             self.HPCC_PORT,
             self.DUMMY_USERNAME,
@@ -67,7 +69,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_verified method with an invalid IP address
     def test_get_verified_invalid_ip(self):
-        test = auth(
+        test = Auth(
             self.DUMMY_HPCC_HOST,
             self.HPCC_PORT,
             self.HPCC_USERNAME,
@@ -79,7 +81,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_verified method with an invalid port
     def test_get_verified_invalid_port(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST,
             self.DUMMY_HPCC_PORT,
             self.HPCC_USERNAME,
@@ -91,7 +93,7 @@ class TestAuth(unittest.TestCase):
 
     # Test the get_verified method with only required parameters
     def test_get_verified_only_required_parameters(self):
-        test = auth(
+        test = Auth(
             self.HPCC_HOST, self.HPCC_PORT, self.HPCC_USERNAME, self.HPCC_PASSWORD
         )
         self.assertTrue(test.get_verified())
